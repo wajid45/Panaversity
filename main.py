8b1ec14aacc0a26a8b078e64a5ab1b548f7eefda
Wajid Ali <111479417+wajid45@users.noreply.github.com>
2024-10-02 19:30:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+# Calculating the sum of 'first_number' and 'second_number'
+sum = first_number + second_number
+print("Sum is: ", sum)
+
+# Calculating the difference
+sub = first_number - second_number
+print("Sub is: ", sub)
+
+# Calculating the division
+div = first_number / second_number
+print("Division of numbers",div)
+
+# Calculating the multiplication
+mul = first_number * second_number
+print("Multipliction of numbers",mul)
+
+# Calculating the exponentiation
+pow = first_number ** second_number
+print("Exponential of numbers",pow)
